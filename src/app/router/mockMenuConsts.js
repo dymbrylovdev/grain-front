71ce3858a170ff09f14abae513afcfdb6e2a8e74
Natkarock <natkarock@gmail.com>
2020-02-05 15:48:47 +0300

    Initial commit

---
+++ src/app/router/mockMenuConsts.js
@@ -0,0 +1,22 @@
+const mockcCereals = [
+  {
+    title: "Гречиха",
+    root: true,
+    page: "",
+    translate: "Гречиха"
+  },
+  {
+    title: "Кукуруза",
+    root: true,
+    page: "",
+    translate: "Кукуруза"
+  },
+  {
+    title: "Люпин",
+    root: true,
+    page: "",
+    translate: "Люпин"
+  }
+];
+
+export { mockcCereals };
