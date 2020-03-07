174c28d4b2cbcf4930bb805af557427933344695
Александр Бахтияров <bakhaa101@gmail.com>
2020-03-07 20:14:43 +0500

    BASE_URL

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://grain.me-interactive.net";
+export const BASE_URL = "https://grain12.me-interactive.net";
 export const API_SEARCH_LOCATION_URI =
   "https://geocode-maps.yandex.ru/1.x/?apikey=c1170a31-17b7-4be1-8db7-e068998ee7cd";
