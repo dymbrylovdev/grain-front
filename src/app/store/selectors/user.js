8419132b3f4d0abd921dab2174172e61d2835f88
Natkarock <natkarock@gmail.com>
2020-03-13 12:41:42 +0300

    fix

--- src/app/store/selectors/user.js
+++ src/app/store/selectors/user.js
@@ -2,6 +2,9 @@ import { useSelector, shallowEqual } from "react-redux";
 
 function UserSelector(id) {
   const user = useSelector(({ users: { users: { data: users} } }) => {
+    if(!id){
+      return {user: {}};
+    }
     const myUser = users.filter(item => item.id === Number.parseInt(id));
     if (myUser.length > 0) {
       return myUser[0];
