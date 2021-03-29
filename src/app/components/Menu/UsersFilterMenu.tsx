63612ff89c806adae41cd25abfd544674ebe34ac
norflin321 <norflin321@gmail.com>
2021-03-29 20:49:22 +0300

    remove menu input in /user-list

--- src/app/components/Menu/UsersFilterMenu.tsx
+++ src/app/components/Menu/UsersFilterMenu.tsx
@@ -9,18 +9,6 @@ interface IUserFilterMenu {
 const UsersFilterMenu: React.FC<IUserFilterMenu> = ({ intl, }): ReactElement => {
   return (
     <div>
-      <TextField
-        select
-        margin="normal"
-        label={intl.formatMessage({ id: "PROFILE.INPUT.ROLE" })}
-        value={"value"}
-        onChange={(e) => console.log(e)}
-        name="role"
-        variant="outlined"
-        // helperText={touched.role && errors.role}
-        // error={Boolean(touched.role && errors.role)}
-        // disabled={editMode !== "create"}
-      />
     </div>
   );
 };
