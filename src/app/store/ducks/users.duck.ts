7ffef42340be712b604aa856a5411ac359079bb7
Nikita Cherepanov <nha.skjrs@gmail.com>
2021-05-03 15:40:50 +0300

    Add user redux boilerplate for email / phone search

--- src/app/store/ducks/users.duck.ts
+++ src/app/store/ducks/users.duck.ts
@@ -81,6 +81,14 @@ const CLEAR_CREATE_USER_FILTER = "users/CLEAR_CREATE_USER_FILTER";
 const CREATE_USER_FILTER_REQUEST = "users/CREATE_USER_FILTER_REQUEST";
 const CREATE_USER_FILTER_SUCCESS = "users/CREATE_USER_FILTER_SUCCESS";
 const CREATE_USER_FILTER_FAIL = "users/CREATE_USER_FILTER_FAIL";
+
+const CLEAR_USER_FILTERS = "users/CLEAR_USER_FILTERS";
+const USER_FILTERS_REQUEST = "users/USER_FILTERS_REQUEST";
+const USER_FILTERS_SUCCESS = "users/USER_FILTERS_SUCCESS";
+const USER_FILTERS_FAIL = "users/USER_FILTERS_FAIL";
+
+const SET_USER_FILTERS_EMAIL = "users/SET_USER_FILTERS_EMAIL";
+const SET_USER_FILTERS_PHONE = "users/SET_USER_FILTERS_PHONE";
 export interface IInitialState {
   page: number;
   per_page: number;
@@ -140,6 +148,13 @@ export interface IInitialState {
   createUserFilterLoading: boolean;
   createUserFilterSuccess: boolean;
   createUserFilterError: string | null;
+
+  userFiltersLoading: boolean;
+  userFiltersSuccess: boolean;
+  userFiltersError: string | null;
+
+  userFiltersEmail: string | undefined;
+  userFiltersPhone: string | undefined;
 }
 
 const initialState: IInitialState = {
@@ -200,6 +215,13 @@ const initialState: IInitialState = {
   createUserFilterLoading: false,
   createUserFilterSuccess: false,
   createUserFilterError: null,
+
+  userFiltersLoading: false,
+  userFiltersSuccess: false,
+  userFiltersError: null,
+
+  userFiltersEmail: undefined,
+  userFiltersPhone: undefined,
 };
 
 export const reducer: Reducer<IInitialState, TAppActions> = (state = initialState, action) => {
@@ -536,6 +558,56 @@ export const reducer: Reducer<IInitialState, TAppActions> = (state = initialStat
       }
     }
 
+    case CLEAR_USER_FILTERS: {
+      return {
+        ...state,
+        userFiltersLoading: false,
+        userFiltersSuccess: false,
+        userFitlersError: null
+      }
+    }
+  
+    case USER_FILTERS_REQUEST: {
+      return {
+        ...state,
+        users: undefined,
+        userFiltersLoading: true,
+        userFiltersSuccess: false,
+        userFitlersError: null
+      }
+    }
+  
+    case USER_FILTERS_SUCCESS: {
+      return {
+        ...state,
+        users: action.payload.data,
+        userFiltersLoading: false,
+        userFiltersSuccess: true,
+      }
+    }
+  
+    case USER_FILTERS_FAIL: {
+      return {
+        ...state,
+        userFiltersLoading: false,
+        userFiltersError: action.payload,
+      }
+    }
+  
+    case SET_USER_FILTERS_EMAIL: {
+      return {
+        ...state,
+        userFiltersEmail: action.payload
+      }
+    }
+  
+    case SET_USER_FILTERS_PHONE: {
+      return {
+        ...state,
+        userFiltersPhone: action.payload
+      }
+    }
+
     default:
       return state;
   }
@@ -603,6 +675,14 @@ export const actions = {
   createUserFilterRequest: (payload: any) => createAction(CREATE_USER_FILTER_REQUEST, payload),
   createUserFilterSuccess: (payload: any) => createAction(CREATE_USER_FILTER_SUCCESS),
   createUserFilterFail: (payload: string) => createAction(CREATE_USER_FILTER_FAIL, payload),
+
+  clearUserFilters: () => createAction(CLEAR_USER_FILTERS),
+  userFiltersRequest: (payload: { email?: string, phone?: string }) => createAction(USER_FILTERS_REQUEST, payload),
+  userFiltersSuccess: (payload: any) => createAction(USER_FILTERS_SUCCESS, payload),
+  userFiltersFail: (payload: string) => createAction(USER_FILTERS_FAIL, payload),
+
+  setUserFiltersEmail: (payload: string) => createAction(SET_USER_FILTERS_EMAIL, payload),
+  setUserFiltersPhone: (payload: string) => createAction(SET_USER_FILTERS_PHONE, payload),
 };
 
 export type TActions = ActionsUnion<typeof actions>;
@@ -711,6 +791,15 @@ function* createUserFilterSaga({ payload }: { payload: { id: number; data: IFilt
   }
 }
 
+function* userFiltersSaga({ payload }: { payload: { email?: string; phone?: string } }) {
+  try {
+    const { data }: { data: any } = yield call(() => getUserFilters(payload.email, payload.phone));
+    yield put(actions.userFiltersSuccess(data));
+  } catch (e) {
+    yield put(actions.userFiltersFail(e?.response?.data?.message) || "Ошибка соединения.");
+  }
+}
+
 export function* saga() {
   yield takeLatest<ReturnType<typeof actions.fetchRequest>>(FETCH_REQUEST, fetchSaga);
   yield takeLatest<ReturnType<typeof actions.fetchByIdRequest>>(FETCH_BY_ID_REQUEST, fetchByIdSaga);
@@ -729,4 +818,5 @@ export function* saga() {
   yield takeLatest<ReturnType<typeof actions.userRolesRequest>>(USER_ROLES_REQUEST, userRolesSaga);
   yield takeLatest<ReturnType<typeof actions.userBidFiltersRequest>>(USER_BID_FILTERS_REQUEST, userBidFiltersSaga);
   yield takeLatest<ReturnType<typeof actions.createUserFilterRequest>>(CREATE_USER_FILTER_REQUEST, createUserFilterSaga);
+  yield takeLatest<ReturnType<typeof actions.userFiltersRequest>>(USER_FILTERS_REQUEST, userFiltersSaga);
 }
