77d7b62a7178e3720c314501a28ebdc8890e1e59
ivan_ryzhov <i.ryzhov@oblakogroup.ru>
2021-11-19 11:50:26 +0300

    full image

---
+++ src/app/pages/home/bids/components/imageDialog/imageDialog.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Dialog, DialogContent, DialogProps, IconButton, makeStyles } from "@material-ui/core";
+import ArrowBack from "@material-ui/icons/ArrowBackIos";
+import ArrowNext from "@material-ui/icons/ArrowForwardIos";
+
+const useStyles = makeStyles(theme => ({
+  img: {
+    // maxWidth: 1000,
+    // // maxHeight: 800,
+    // [theme.breakpoints.down("xs")]: {
+    //   maxWidth: 250,
+    // },
+    // [theme.breakpoints.only("sm")]: {
+    //   maxWidth: 500,
+    // },
+    width: "100%",
+    objectFit: "contain",
+  },
+  backdrop: {
+    backdropFilter: "blur(3px)",
+    backgroundColor: "rgba(0,0,30,0.4)",
+  },
+  arrows: {
+    position: "absolute",
+    width: "98%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    overflow: "hidden",
+  },
+}));
+
+interface IProps extends DialogProps {
+  handleClose: any;
+  url: string;
+  handleArrow: any;
+}
+
+const ImageDialog: React.FC<IProps> = ({ open, handleClose, url, handleArrow }) => {
+  const classes = useStyles();
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      maxWidth="lg"
+      BackdropProps={{
+        classes: {
+          root: classes.backdrop,
+        },
+      }}
+    >
+      <DialogContent style={{ padding: 0, overflow: "hidden" }}>
+        <div className={classes.arrows}>
+          <IconButton
+            onClick={() => handleArrow("prev")}
+            style={{ top: "50%", transform: "translateY(-50%)", zIndex: 2, color: "#fff", left: 15 }}
+          >
+            <ArrowBack style={{ fontSize: 40 }} />
+          </IconButton>
+          <IconButton
+            onClick={() => handleArrow("next")}
+            style={{ top: "50%", transform: "translateY(-50%)", zIndex: 2, color: "#fff", left: 0 }}
+          >
+            <ArrowNext style={{ fontSize: 40 }} />
+          </IconButton>
+        </div>
+        <img className={classes.img} src={url} />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ImageDialog;
--- src/app/pages/home/bids/components/imageGallery/ImageGallery.tsx
+++ src/app/pages/home/bids/components/imageGallery/ImageGallery.tsx
@@ -1,15 +1,14 @@
-import React, { useMemo, useState } from "react";
-import { Card, IconButton, makeStyles } from "@material-ui/core";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { Card, CardMedia, IconButton, makeStyles } from "@material-ui/core";
 import "react-image-gallery/styles/css/image-gallery.css";
 import ImageGalleryLib from "react-image-gallery";
 import ArrowBack from "@material-ui/icons/ArrowBackIos";
 import ArrowNext from "@material-ui/icons/ArrowForwardIos";
 import { toAbsoluteUrl } from "../../../../../../_metronic/utils/utils";
+import ImageDialog from "../imageDialog/imageDialog";
 // import { API_DOMAIN } from "../../../../../constants";
 
-interface IProps {
-  //   product: any;
-}
+interface IProps {}
 
 const useStyles = makeStyles(theme => ({
   imgContainer: {
@@ -42,6 +41,20 @@ const useStyles = makeStyles(theme => ({
     transform: "translateY(-50%)",
     zIndex: 2,
     color: "#fff",
+  },
+  wrapperBtnFull: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    maxWidth: 530,
+    width: "100%",
+    height: 311,
+    zIndex: 1,
+    // eslint-disable-next-line no-useless-computed-key
+    ["@media (max-width:1000px)"]: {
+      paddingRight: 20,
+    },
   },
 }));
 
@@ -94,11 +107,17 @@ const photos = [
 
 const ImageGallery: React.FC<IProps> = () => {
   const classes = useStyles();
+  const imageGalleryRef: any = useRef();
   const [isFullScreen, setIsFullScreen] = useState(false);
+  const [showFullImage, setShowFullImage] = useState(false);
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    setCurrentImage(photos[0].path);
+  }, [photos, setCurrentImage]);
 
   // images
   const images = useMemo(() => {
-    // if (product?.photos) {
     return photos.map(el => ({
       original: el.path,
       thumbnail: el.small,
@@ -111,46 +130,71 @@ const ImageGallery: React.FC<IProps> = () => {
         </Card>
       ),
     }));
-    // } else {
-    //   return null;
-    // }
-    // return []
   }, [isFullScreen, photos, classes]);
 
+  const handleArrow = useCallback(
+    (operation: "prev" | "next") => {
+      const currentIndex: number = imageGalleryRef.current.getCurrentIndex();
+      const maxIndex = photos.length - 1;
+      if (operation === "next") {
+        currentIndex === maxIndex ? imageGalleryRef.current.slideToIndex(0) : imageGalleryRef.current.slideToIndex(currentIndex + 1);
+      }
+      if (operation === "prev") {
+        currentIndex === 0 ? imageGalleryRef.current.slideToIndex(maxIndex) : imageGalleryRef.current.slideToIndex(currentIndex - 1);
+      }
+    },
+    [photos, imageGalleryRef]
+  );
+
   return (
-    <div className={classes.galleryContainer}>
-      {images && (
-        <>
-          {images.length > 0 ? (
-            <ImageGalleryLib
-              onScreenChange={boolean => setIsFullScreen(boolean)}
-              items={images}
-              useBrowserFullscreen
-              showThumbnails={images.length > 1}
-              showFullscreenButton
-              disableThumbnailScroll={false}
-              showPlayButton={false}
-              thumbnailPosition="bottom"
-              infinite
-              renderLeftNav={onClick => (
-                <IconButton onClick={onClick} className={classes.arrow} style={{ left: 15 }}>
-                  <ArrowBack style={{ fontSize: 30 }} />
-                </IconButton>
-              )}
-              renderRightNav={onClick => (
-                <IconButton onClick={onClick} className={classes.arrow} style={{ right: 15 }}>
-                  <ArrowNext style={{ fontSize: 30 }} />
-                </IconButton>
-              )}
-            />
-          ) : (
-            <Card className={classes.imgContainer} elevation={0}>
-              <img className={classes.img} src={toAbsoluteUrl("/images/defaultImage.jpg")} />
-            </Card>
-          )}
-        </>
+    <>
+      {currentImage && (
+        <ImageDialog open={showFullImage} url={currentImage} handleClose={() => setShowFullImage(false)} handleArrow={handleArrow} />
       )}
-    </div>
+      <div className={classes.galleryContainer}>
+        <div className={classes.wrapperBtnFull}>
+          <CardMedia
+            component="img"
+            title="image"
+            image={toAbsoluteUrl("/images/isFullImage.png")}
+            style={{ objectFit: "none", width: 100, height: 100, cursor: "pointer" }}
+            onClick={() => setShowFullImage(true)}
+          />
+        </div>
+        {images && (
+          <>
+            {images.length > 0 ? (
+              <ImageGalleryLib
+                ref={imageGalleryRef}
+                onScreenChange={boolean => setIsFullScreen(boolean)}
+                items={images}
+                showThumbnails={images.length > 1}
+                showFullscreenButton={false}
+                disableThumbnailScroll={false}
+                showPlayButton={false}
+                thumbnailPosition="bottom"
+                infinite
+                onSlide={index => setCurrentImage(photos[index].path)}
+                renderLeftNav={onClick => (
+                  <IconButton onClick={onClick} className={classes.arrow} style={{ left: 15, zIndex: 2 }}>
+                    <ArrowBack style={{ fontSize: 30 }} />
+                  </IconButton>
+                )}
+                renderRightNav={onClick => (
+                  <IconButton onClick={onClick} className={classes.arrow} style={{ right: 15, zIndex: 2 }}>
+                    <ArrowNext style={{ fontSize: 30 }} />
+                  </IconButton>
+                )}
+              />
+            ) : (
+              <Card className={classes.imgContainer} elevation={0}>
+                <img className={classes.img} src={toAbsoluteUrl("/images/defaultImage.jpg")} />
+              </Card>
+            )}
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
