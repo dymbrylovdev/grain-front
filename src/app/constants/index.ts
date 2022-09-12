25f6f82922955ed740675eef13df22f9cc7f4c75
laptevlife <laptevlife@gmail.com>
2022-09-12 18:03:14 +0400

    gr-922

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,9 +1,10 @@
 const {
-  REACT_APP_API_DOMAIN = "grain-api.me-interactive.net",
+  // REACT_APP_API_DOMAIN = "grain-api.me-interactive.net",
+  REACT_APP_API_DOMAIN = "grain-apidev.me-interactive.net",
   REACT_APP_GOOGLE_API_KEY = "c1170a31-17b7-4be1-8db7-e068998ee7cd",
 } = process.env;
 
-const { protocol } = window.location;
+const protocol = "https:";
 const API_DOMAIN = `${protocol}//${REACT_APP_API_DOMAIN}`;
 
 const API_SEARCH_LOCATION_URI = `https://geocode-maps.yandex.ru/1.x/?apikey=${REACT_APP_GOOGLE_API_KEY}`;
