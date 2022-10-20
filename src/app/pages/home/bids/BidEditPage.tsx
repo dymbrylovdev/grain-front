e04c3febac077ff200be0b7e3b891cc9733dc74b
laptevlife <laptevlife@gmail.com>
2022-10-20 18:54:33 +0400

    fix: bid reload

--- src/app/pages/home/bids/BidEditPage.tsx
+++ src/app/pages/home/bids/BidEditPage.tsx
@@ -35,316 +35,318 @@ const BidEditPage: React.FC<TPropsFromRedux &
     cropId: string;
     vendorId: string;
   }>> = ({
-  match: {
-    params: { editMode, salePurchaseMode, bidId, cropId, vendorId },
-  },
-  match,
+    match: {
+      params: { editMode, salePurchaseMode, bidId, cropId, vendorId },
+    },
+    match,
 
-  intl,
+    intl,
 
-  fetchMe,
-  me,
+    fetchMe,
+    me,
 
-  fetchUser,
-  user,
-  userLoading,
-  userError,
+    fetchUser,
+    user,
+    userLoading,
+    userError,
 
-  setActiveStep,
+    setActiveStep,
 
-  clearFetch,
-  fetch,
-  bid,
-  loading,
-  error,
+    clearFetch,
+    fetch,
+    bid,
+    loading,
+    error,
 
-  clearBidsPair,
-  fetchBidsPair,
-  bidsPair,
-  bidsPairLoading,
-  bidsPairSuccess,
-  bidsPairError,
+    clearBidsPair,
+    fetchBidsPair,
+    bidsPair,
+    bidsPairLoading,
+    bidsPairSuccess,
+    bidsPairError,
 
-  fetchCrops,
-  crops,
-  cropsLoading,
-  cropsError,
+    fetchCrops,
+    crops,
+    cropsLoading,
+    cropsError,
 
-  clearCropParams,
-  fetchCropParams,
-  cropParams,
-  cropParamsLoading,
-  cropParamsError,
+    clearCropParams,
+    fetchCropParams,
+    cropParams,
+    cropParamsLoading,
+    cropParamsError,
 
-  clearCreate,
-  create,
-  createLoading,
-  createSuccess,
-  createError,
-  clearDel,
-  del,
-  delLoading,
-  delSuccess,
-  delError,
-  clearEdit,
-  edit,
-  editLoading,
-  editSuccess,
-  editError,
+    clearCreate,
+    create,
+    createLoading,
+    createSuccess,
+    createError,
+    clearDel,
+    del,
+    delLoading,
+    delSuccess,
+    delError,
+    clearEdit,
+    edit,
+    editLoading,
+    editSuccess,
+    editError,
 
-  clearPost,
-  post,
-  postLoading,
-  postSuccess,
-  postError,
+    clearPost,
+    post,
+    postLoading,
+    postSuccess,
+    postError,
 
-  editContactViewCount,
+    editContactViewCount,
 
-  fetchLocations,
-  clearLocations,
-  locations,
-  loadingLocations,
+    fetchLocations,
+    clearLocations,
+    locations,
+    loadingLocations,
 
-  profit,
+    profit,
 
-  currentSaleFilters,
-  currentPurchaseFilters,
+    currentSaleFilters,
+    currentPurchaseFilters,
 
-  openInfoAlert,
-  setOpenInfoAlert,
+    openInfoAlert,
+    setOpenInfoAlert,
 
-  fetchFilters,
-  filterCount,
+    fetchFilters,
+    filterCount,
 
-  usersCropsLoading,
-  usersCropsSuccess,
-  usersCropsError,
+    usersCropsLoading,
+    usersCropsSuccess,
+    usersCropsError,
 
-  clearUsersCrops,
-  fetchUsersCrops,
-  // fetchTransporters,
-}) => {
-  const isNoModerate = !vendorId && !+bidId && me?.status === "На модерации";
-  const classes = useStyles();
-  const history = useHistory();
-  const [isAlertOpen, setAlertOpen] = useState(false);
-  const [pointPrices, setPointPrices] = useState<IPointPriceForGet[]>([]);
-  const guestPoint = useMemo(() => {
-    const localPoint = getPoint();
-    return localPoint.active && !me ? localPoint : undefined;
-  }, [me]);
+    clearUsersCrops,
+    fetchUsersCrops,
+    // fetchTransporters,
+  }) => {
+    const isNoModerate = !vendorId && !+bidId && me?.status === "На модерации";
+    const classes = useStyles();
+    const history = useHistory();
+    const [isAlertOpen, setAlertOpen] = useState(false);
+    const [pointPrices, setPointPrices] = useState<IPointPriceForGet[]>([]);
+    const guestPoint = useMemo(() => {
+      const localPoint = getPoint();
+      return localPoint.active && !me ? localPoint : undefined;
+    }, [me]);
 
-  useEffect(() => {
-    const currentFilters = salePurchaseMode === "sale" ? currentSaleFilters : currentPurchaseFilters;
-    let currentFilter: { [crop: string]: { [x: string]: any } } | undefined = undefined;
-    if (editMode === "view") {
-      for (let k in currentFilters) {
-        if (k === cropId) {
-          currentFilter = currentFilters[k];
+    useEffect(() => {
+      const currentFilters = salePurchaseMode === "sale" ? currentSaleFilters : currentPurchaseFilters;
+      let currentFilter: { [crop: string]: { [x: string]: any } } | undefined = undefined;
+      if (editMode === "view") {
+        for (let k in currentFilters) {
+          if (k === cropId) {
+            currentFilter = currentFilters[k];
+          }
         }
       }
-    }
-    const pointPrices: IPointPriceForGet[] = [];
-    if (currentFilter?.point_prices && currentFilter.point_prices.length) {
-      currentFilter.point_prices.forEach((item: any) => {
-        pointPrices.push(item);
-      });
-    }
-    setPointPrices(pointPrices);
-  }, [salePurchaseMode, currentSaleFilters, currentPurchaseFilters, editMode, cropId]);
+      const pointPrices: IPointPriceForGet[] = [];
+      if (currentFilter?.point_prices && currentFilter.point_prices.length) {
+        currentFilter.point_prices.forEach((item: any) => {
+          pointPrices.push(item);
+        });
+      }
+      setPointPrices(pointPrices);
+    }, [salePurchaseMode, currentSaleFilters, currentPurchaseFilters, editMode, cropId]);
 
-  const { enqueueSnackbar } = useSnackbar();
+    const { enqueueSnackbar } = useSnackbar();
 
-  useEffect(() => {
-    if (delSuccess || delError) {
-      enqueueSnackbar(
-        delSuccess
-          ? intl.formatMessage({ id: "NOTISTACK.BIDS.DEL.OK" })
-          : `${intl.formatMessage({ id: "NOTISTACK.ERRORS.ERROR" })} ${delError}`,
-        {
-          variant: delSuccess ? "success" : "error",
-        }
-      );
-      setAlertOpen(false);
-      clearDel();
-      if (delSuccess) history.goBack();
-    }
-  }, [clearDel, delError, delSuccess, enqueueSnackbar, history, intl]);
+    useEffect(() => {
+      if (delSuccess || delError) {
+        enqueueSnackbar(
+          delSuccess
+            ? intl.formatMessage({ id: "NOTISTACK.BIDS.DEL.OK" })
+            : `${intl.formatMessage({ id: "NOTISTACK.ERRORS.ERROR" })} ${delError}`,
+          {
+            variant: delSuccess ? "success" : "error",
+          }
+        );
+        setAlertOpen(false);
+        clearDel();
+        if (delSuccess) history.goBack();
+      }
+    }, [clearDel, delError, delSuccess, enqueueSnackbar, history, intl]);
 
-  useEffect(() => {
-    if (editSuccess || editError) {
-      enqueueSnackbar(
-        editSuccess
-          ? intl.formatMessage({ id: "NOTISTACK.BIDS.EDIT" })
-          : `${intl.formatMessage({ id: "NOTISTACK.ERRORS.ERROR" })} ${editError}`,
-        {
-          variant: editSuccess ? "success" : "error",
-        }
-      );
-      clearEdit();
-    }
-  }, [clearEdit, editError, editSuccess, enqueueSnackbar, history, intl]);
+    useEffect(() => {
+      if (editSuccess || editError) {
+        enqueueSnackbar(
+          editSuccess
+            ? intl.formatMessage({ id: "NOTISTACK.BIDS.EDIT" })
+            : `${intl.formatMessage({ id: "NOTISTACK.ERRORS.ERROR" })} ${editError}`,
+          {
+            variant: editSuccess ? "success" : "error",
+          }
+        );
+        clearEdit();
+      }
+    }, [clearEdit, editError, editSuccess, enqueueSnackbar, history, intl]);
 
-  useEffect(() => {
-    if (vendorId) {
-      fetchUsersCrops(+vendorId);
-    } else {
-      fetchCrops();
-    }
-  }, [fetchCrops, fetchUsersCrops, vendorId]);
-
-  useEffect(() => {
-    if (+bidId) fetch(+bidId, { filter: { point_prices: pointPrices, location: guestPoint } });
-    return () => {
-      clearFetch();
-    };
-  }, [bidId, clearFetch, fetch, pointPrices, guestPoint]);
-
-  useEffect(() => {
-    if (!bid) {
+    useEffect(() => {
       if (vendorId) {
-        fetchUser({ id: +vendorId });
+        fetchUsersCrops(+vendorId);
+      } else {
+        fetchCrops();
       }
-    } else {
-      fetchUser({ id: bid.vendor.id });
+    }, [fetchCrops, fetchUsersCrops, vendorId]);
+
+    useEffect(() => {
+      if (+bidId) fetch(+bidId, { filter: { point_prices: pointPrices, location: guestPoint } });
+      return () => {
+        clearFetch();
+      };
+    }, [bidId, clearFetch, fetch, pointPrices, guestPoint]);
+
+    useEffect(() => {
+      if (me) {
+        if (!bid) {
+          if (vendorId) {
+            fetchUser({ id: +vendorId });
+          }
+        } else {
+          fetchUser({ id: bid.vendor.id });
+        }
+      }
+    }, [bid, fetchUser, vendorId, me]);
+
+    useEffect(() => {
+      setActiveStep(4);
+    }, [setActiveStep]);
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+      fetchMe();
+    }, [fetchMe]);
+
+    let title = "string";
+
+    if (editMode === "create" && !vendorId) title = intl.formatMessage({ id: "BID.TITLE.CREATE" });
+    if (editMode === "create" && !!vendorId && !!user && user.id === +vendorId)
+      title = `${intl.formatMessage({ id: "BID.TITLE.BY_VENDOR" })} [ ${user.login} ]`;
+    if (editMode === "view") title = intl.formatMessage({ id: "BID.TITLE.VIEW" });
+    if (editMode === "edit") title = intl.formatMessage({ id: "BID.TITLE.EDIT" });
+    if (!+bidId && (editMode === "edit" || editMode === "view")) return <ErrorPage />;
+
+    if (!(editMode === "create" || editMode === "edit" || editMode === "view")) return <ErrorPage />;
+
+    if (!(salePurchaseMode === "sale" || salePurchaseMode === "purchase")) return <ErrorPage />;
+    if (editMode === "create" && me?.is_buyer && salePurchaseMode === "sale") return <ErrorPage />;
+
+    if (editMode === "create" && me?.is_vendor && salePurchaseMode === "purchase") return <ErrorPage />;
+
+    if (editMode === "edit" && me && !accessByRoles(me, ["ROLE_ADMIN", "ROLE_MANAGER"]) && !!bid && bid.vendor.id !== me?.id)
+      return <ErrorPage />;
+
+    if (!!user && !!vendorId && user.id === +vendorId && editMode === "create" && user.is_buyer && salePurchaseMode !== "purchase")
+      return <ErrorPage />;
+
+    if (!!user && !!vendorId && user.id === +vendorId && editMode === "create" && user.is_vendor && salePurchaseMode !== "sale")
+      return <ErrorPage />;
+
+    if (error || userError || cropsError || usersCropsError || cropParamsError) {
+      setTimeout(() => {
+        window.location.reload();
+      }, 10000);
     }
-  }, [bid, fetchUser, vendorId]);
 
-  useEffect(() => {
-    setActiveStep(4);
-  }, [setActiveStep]);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-    fetchMe();
-  }, [fetchMe]);
-
-  let title = "string";
-
-  if (editMode === "create" && !vendorId) title = intl.formatMessage({ id: "BID.TITLE.CREATE" });
-  if (editMode === "create" && !!vendorId && !!user && user.id === +vendorId)
-    title = `${intl.formatMessage({ id: "BID.TITLE.BY_VENDOR" })} [ ${user.login} ]`;
-  if (editMode === "view") title = intl.formatMessage({ id: "BID.TITLE.VIEW" });
-  if (editMode === "edit") title = intl.formatMessage({ id: "BID.TITLE.EDIT" });
-  if (!+bidId && (editMode === "edit" || editMode === "view")) return <ErrorPage />;
-
-  if (!(editMode === "create" || editMode === "edit" || editMode === "view")) return <ErrorPage />;
-
-  if (!(salePurchaseMode === "sale" || salePurchaseMode === "purchase")) return <ErrorPage />;
-  if (editMode === "create" && me?.is_buyer && salePurchaseMode === "sale") return <ErrorPage />;
-
-  if (editMode === "create" && me?.is_vendor && salePurchaseMode === "purchase") return <ErrorPage />;
-
-  if (editMode === "edit" && me && !accessByRoles(me, ["ROLE_ADMIN", "ROLE_MANAGER"]) && !!bid && bid.vendor.id !== me?.id)
-    return <ErrorPage />;
-
-  if (!!user && !!vendorId && user.id === +vendorId && editMode === "create" && user.is_buyer && salePurchaseMode !== "purchase")
-    return <ErrorPage />;
-
-  if (!!user && !!vendorId && user.id === +vendorId && editMode === "create" && user.is_vendor && salePurchaseMode !== "sale")
-    return <ErrorPage />;
-
-  if (error || userError || cropsError || usersCropsError || cropParamsError) {
-    setTimeout(() => {
-      window.location.reload();
-    }, 10000);
-  }
-
-  return (
-    <>
-      {editMode === "view" ? (
-        <ViewBidForm
-          intl={intl}
-          vendorId={+vendorId}
-          user={user}
-          salePurchaseMode={salePurchaseMode}
-          editMode={editMode}
-          cropId={+cropId}
-          crops={crops}
-          bid={bid}
-          me={me}
-          fetchCropParams={fetchCropParams}
-          cropParams={cropParams}
-          guestPoint={guestPoint}
+    return (
+      <>
+        {editMode === "view" ? (
+          <ViewBidForm
+            intl={intl}
+            vendorId={+vendorId}
+            user={user}
+            salePurchaseMode={salePurchaseMode}
+            editMode={editMode}
+            cropId={+cropId}
+            crops={crops}
+            bid={bid}
+            me={me}
+            fetchCropParams={fetchCropParams}
+            cropParams={cropParams}
+            guestPoint={guestPoint}
           // fetchTransporters={fetchTransporters}
-        />
-      ) : (
-        <>
-          <Prompter />
-          <Paper className={classes.paperWithForm}>
-            {title && <LayoutSubheader title={title} />}
-            {isNoModerate ? (
-              <div className={classes.titleText}>{intl.formatMessage({ id: "BID.STATUS.NO_MODERATE" })}</div>
-            ) : (
-              <BidForm
-                intl={intl}
-                vendorId={+vendorId}
-                user={user}
-                editMode={editMode}
-                salePurchaseMode={salePurchaseMode}
-                cropId={+cropId}
-                crops={crops}
-                bid={editMode === "create" ? undefined : bid}
-                bidsPair={bidsPair}
-                fetchMe={fetchMe}
-                me={me}
-                fetchFilters={fetchFilters}
-                filterCount={filterCount}
-                fetchLocations={fetchLocations}
-                locations={locations}
-                clearBidsPair={clearBidsPair}
-                fetchBidsPair={fetchBidsPair}
-                bidsPairSuccess={bidsPairSuccess}
-                bidsPairError={bidsPairError}
-                bidsPairLoading={bidsPairLoading}
-                loadingLocations={loadingLocations}
-                clearLocations={clearLocations}
-                clearCropParams={clearCropParams}
-                fetchCropParams={fetchCropParams}
-                cropParams={cropParams}
-                cropParamsLoading={cropParamsLoading}
-                setAlertOpen={setAlertOpen}
-                buttonLoading={createLoading || editLoading || postLoading}
-                create={create}
-                createSuccess={createSuccess}
-                createError={createError}
-                clearCreate={clearCreate}
-                post={post}
-                postSuccess={postSuccess}
-                postError={postError}
-                clearPost={clearPost}
-                edit={edit}
-                profit={profit}
-                openInfoAlert={openInfoAlert}
-                setOpenInfoAlert={setOpenInfoAlert}
-                editContactViewCount={editContactViewCount}
-                pointPrices={pointPrices}
-                editSuccess={editSuccess}
-                fetch={fetch}
+          />
+        ) : (
+          <>
+            <Prompter />
+            <Paper className={classes.paperWithForm}>
+              {title && <LayoutSubheader title={title} />}
+              {isNoModerate ? (
+                <div className={classes.titleText}>{intl.formatMessage({ id: "BID.STATUS.NO_MODERATE" })}</div>
+              ) : (
+                <BidForm
+                  intl={intl}
+                  vendorId={+vendorId}
+                  user={user}
+                  editMode={editMode}
+                  salePurchaseMode={salePurchaseMode}
+                  cropId={+cropId}
+                  crops={crops}
+                  bid={editMode === "create" ? undefined : bid}
+                  bidsPair={bidsPair}
+                  fetchMe={fetchMe}
+                  me={me}
+                  fetchFilters={fetchFilters}
+                  filterCount={filterCount}
+                  fetchLocations={fetchLocations}
+                  locations={locations}
+                  clearBidsPair={clearBidsPair}
+                  fetchBidsPair={fetchBidsPair}
+                  bidsPairSuccess={bidsPairSuccess}
+                  bidsPairError={bidsPairError}
+                  bidsPairLoading={bidsPairLoading}
+                  loadingLocations={loadingLocations}
+                  clearLocations={clearLocations}
+                  clearCropParams={clearCropParams}
+                  fetchCropParams={fetchCropParams}
+                  cropParams={cropParams}
+                  cropParamsLoading={cropParamsLoading}
+                  setAlertOpen={setAlertOpen}
+                  buttonLoading={createLoading || editLoading || postLoading}
+                  create={create}
+                  createSuccess={createSuccess}
+                  createError={createError}
+                  clearCreate={clearCreate}
+                  post={post}
+                  postSuccess={postSuccess}
+                  postError={postError}
+                  clearPost={clearPost}
+                  edit={edit}
+                  profit={profit}
+                  openInfoAlert={openInfoAlert}
+                  setOpenInfoAlert={setOpenInfoAlert}
+                  editContactViewCount={editContactViewCount}
+                  pointPrices={pointPrices}
+                  editSuccess={editSuccess}
+                  fetch={fetch}
+                />
+              )}
+              <AlertDialog
+                isOpen={isAlertOpen}
+                text={intl.formatMessage({ id: "BIDSLIST.DIALOGS.DELETE_TEXT" })}
+                okText={intl.formatMessage({
+                  id: "USERLIST.DIALOGS.AGREE_TEXT",
+                })}
+                cancelText={intl.formatMessage({
+                  id: "USERLIST.DIALOGS.CANCEL_TEXT",
+                })}
+                handleClose={() => setAlertOpen(false)}
+                handleAgree={() => del(+bidId)}
+                loadingText={intl.formatMessage({
+                  id: "BIDSLIST.DIALOGS.LOADING_TEXT",
+                })}
+                isLoading={delLoading}
               />
-            )}
-            <AlertDialog
-              isOpen={isAlertOpen}
-              text={intl.formatMessage({ id: "BIDSLIST.DIALOGS.DELETE_TEXT" })}
-              okText={intl.formatMessage({
-                id: "USERLIST.DIALOGS.AGREE_TEXT",
-              })}
-              cancelText={intl.formatMessage({
-                id: "USERLIST.DIALOGS.CANCEL_TEXT",
-              })}
-              handleClose={() => setAlertOpen(false)}
-              handleAgree={() => del(+bidId)}
-              loadingText={intl.formatMessage({
-                id: "BIDSLIST.DIALOGS.LOADING_TEXT",
-              })}
-              isLoading={delLoading}
-            />
-          </Paper>
-        </>
-      )}
-    </>
-  );
-};
+            </Paper>
+          </>
+        )}
+      </>
+    );
+  };
 
 const connector = connect(
   (state: IAppState) => ({
