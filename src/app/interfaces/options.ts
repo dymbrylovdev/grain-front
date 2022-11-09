c3c760ba5636840ac2a8a9456f28639f46fb0926
Valery Yudintsev <vaud55@gmail.com>
2022-11-09 19:22:32 +0400

    GRAIN-T-16

--- src/app/interfaces/options.ts
+++ src/app/interfaces/options.ts
@@ -2,6 +2,14 @@ export interface ITransport {
     technical_details: string;
     weight: number;
     amount: number;
+    loading: string;
+    overload: string;
+    nds: boolean;
+    sidewall_height: string;
+    cabin_height: string;
+    length: string;
+    name: string;
+    available: boolean;
     location?: {
         lat: number;
         lng: number;
