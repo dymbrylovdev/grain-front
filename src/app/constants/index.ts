2e652feade003a1dcdfa8cf87014855bf26e3e98
ivan_ryzhov <i.ryzhov@oblakogroup.ru>
2022-11-17 13:05:34 +0300

    yamap key

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -2,7 +2,7 @@ const {
   // REACT_APP_API_DOMAIN = "grain-api.me-interactive.net",
   REACT_APP_API_DOMAIN = "api.kupit-zerno.com",
   // REACT_APP_API_DOMAIN = "grain-apidev.me-interactive.net",
-  REACT_APP_GOOGLE_API_KEY = "c1170a31-17b7-4be1-8db7-e068998ee7cd",
+  REACT_APP_GOOGLE_API_KEY = "7f9da232-66bd-4aa0-97de-928d75b7ea37",
 } = process.env;
 
 const protocol = "https:";
