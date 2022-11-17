f9dbd3ca922205346432d0299fed7696ac316272
ivan_ryzhov <i.ryzhov@oblakogroup.ru>
2022-11-17 12:14:36 +0300

    fixed useEffect, api

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,6 +1,6 @@
 const {
-  // REACT_APP_API_DOMAIN = "grain-api.me-interactive.net",
-  REACT_APP_API_DOMAIN = "api.kupit-zerno.com",
+  REACT_APP_API_DOMAIN = "grain-api.me-interactive.net",
+  // REACT_APP_API_DOMAIN = "api.kupit-zerno.com",
   // REACT_APP_API_DOMAIN = "grain-apidev.me-interactive.net",
   REACT_APP_GOOGLE_API_KEY = "c1170a31-17b7-4be1-8db7-e068998ee7cd",
 } = process.env;
